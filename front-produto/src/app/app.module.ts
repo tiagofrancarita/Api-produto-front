64b1c494dda7a79b261569b4dfd43a0047663204
Tiago França <tiagofranca.rita@gmail.com>
2022-11-19 02:41:05 -0300

    criacao da estrutura de desenvolvimento do front-end em angular 13

--- front-produto/src/app/app.module.ts
+++ front-produto/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-produto/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  url: 'http://apiproduto-env.eba-gjevuctk.us-east-1.elasticbeanstalk.com/api'
+}
